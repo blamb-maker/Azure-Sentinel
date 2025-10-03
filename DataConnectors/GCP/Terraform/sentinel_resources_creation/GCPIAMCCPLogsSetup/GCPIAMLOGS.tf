04b185f2769ae65e9552211b6917f6a898d239c0
blamb-maker <blamb@dodsecurity.com>
2025-10-03 09:00:00 -0600

    Update GCPIAMLOGS.tf

--- DataConnectors/GCP/Terraform/sentinel_resources_creation/GCPIAMCCPLogsSetup/GCPIAMLOGS.tf
+++ DataConnectors/GCP/Terraform/sentinel_resources_creation/GCPIAMCCPLogsSetup/GCPIAMLOGS.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 data "google_project" "project" {
-  project_id = "Your project_id should be entered"
+  project_id = "1025333369233"
 }
 
 variable "topic-name" {
@@ -21,7 +21,7 @@ variable "topic-name" {
 
 variable "organization-id" {
   type    = string
-  default = ""
+  default = "a1e2de66-0364-455c-ae0f-b271970d13cc"
   description = "Organization id"
 }
 
